82aa3555021e78c3ec5496271ccc942c329cfd8e
Andre Powers <andre@stuypend.com>
2021-02-11 21:10:05 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -4,4 +4,6 @@ def game_hash
   team[:colors] = []
   team[:players] = []
 
+  team[:players] = [ {:player_name => ""}, {:number => -1}, {:shoe => -1}, {:points => -1}, {:rebounds => -1}, {:assists => -1}, {:steals => -1}, {:blocks => -1}, {:slam_dunks => -1} ]
+
 end
