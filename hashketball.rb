717149879a0ba8d9cc15337fb62bb603a51a5164
Andre Powers <andre@stuypend.com>
2021-02-11 21:19:11 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -3,12 +3,15 @@ def game_hash
   team[:home] = {}
   team[:away] = {}
 
-  team[:home][:team_name] = ""
-  team[:home][:colors] = []
+  team[:home][:team_name] = "Brooklyn Nets"
+  team[:home][:colors] = ["Black", "White"]
   team[:home][:players] = []
 
-  team[:home][:players] = [ {:player_name => ""}, {:number => -1}, {:shoe => -1}, {:points => -1}, {:rebounds => -1}, {:assists => -1}, {:steals => -1}, {:blocks => -1}, {:slam_dunks => -1} ]
+  team[:home][:players] = [ {:player_name => "", :number => -1, :shoe => -1, :points => -1, :rebounds => -1, :assists => -1, :steals => -1, :blocks => -1, :slam_dunks => -1} ]
+  team[:home][:players][0][:player_name] = "Alan Anderson"
 
+
+  
   team[:away][:team_name] = ""
   team[:away][:colors] = []
   team[:away][:players] = []
