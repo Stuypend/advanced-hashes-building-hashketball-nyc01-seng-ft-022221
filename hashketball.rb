fab53feb78d49a69d84f104034ffbd4d7fc426a6
Andre Powers <andre@stuypend.com>
2021-02-11 21:12:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,14 @@
 def game_hash
   team = {}
-  team[:team_name] = ""
-  team[:colors] = []
-  team[:players] = []
+  team[:home] = {}
+  team[:away] = {}
 
-  team[:players] = [ {:player_name => ""}, {:number => -1}, {:shoe => -1}, {:points => -1}, {:rebounds => -1}, {:assists => -1}, {:steals => -1}, {:blocks => -1}, {:slam_dunks => -1} ]
+  team[:home][:team_name] = ""
+  team[:home][:colors] = []
+  team[:home][:players] = []
 
+  team[:home][:players] = [ {:player_name => ""}, {:number => -1}, {:shoe => -1}, {:points => -1}, {:rebounds => -1}, {:assists => -1}, {:steals => -1}, {:blocks => -1}, {:slam_dunks => -1} ]
+
+
+  return team
 end
