0d32fab3e73879d809306436d0b663dfb8e1d86d
Andre Powers <andre@stuypend.com>
2021-02-11 21:50:24 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -19,7 +19,7 @@ def game_hash
   team[:home][:players][0][:slam_dunks] = 1
 
   team[:home][:players][1] = {:player_name => "Reggie Evans", :number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7}
-  team[:home][:players][2] = {:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 9, :blocks => 1, :slam_dunks => 15}
+  team[:home][:players][2] = {:player_name => "Brook Lopez", :number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15}
   team[:home][:players][3] = {:player_name => "Mason Plumlee	", :number => 1, :shoe => 19, :points => 26, :rebounds => 11, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5}
   team[:home][:players][4] = {:player_name => "Jason Terry", :number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1}
 
