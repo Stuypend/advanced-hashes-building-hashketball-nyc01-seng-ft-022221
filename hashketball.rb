b278fefa2c3df1fd1509316beaf3df5597ab89e3
Andre Powers <andre@stuypend.com>
2021-02-11 21:30:01 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -59,24 +59,24 @@ def game_hash
   team[:home][:players][4][:slam_dunks] = 1
 
   team[:away][:players][0][:player_name] = "Jeff Adrien"
-  team[:away][:players][0][:number] = 0
-  team[:away][:players][0][:shoe] = 16
-  team[:away][:players][0][:points] = 22
-  team[:away][:players][0][:rebounds] = 12
-  team[:away][:players][0][:assists] = 12
-  team[:away][:players][0][:steals] = 3
-  team[:away][:players][0][:blocks] = 1
-  team[:away][:players][0][:slam_dunks] = 1
+  team[:away][:players][0][:number] = 4
+  team[:away][:players][0][:shoe] = 18
+  team[:away][:players][0][:points] = 10
+  team[:away][:players][0][:rebounds] = 1
+  team[:away][:players][0][:assists] = 1
+  team[:away][:players][0][:steals] = 2
+  team[:away][:players][0][:blocks] = 7
+  team[:away][:players][0][:slam_dunks] = 2
 
-  team[:away][:players][1][:player_name] = "Reggie Evans"
-  team[:away][:players][1][:number] = 30
-  team[:away][:players][1][:shoe] = 14
+  team[:away][:players][1][:player_name] = "Bismack Biyombo"
+  team[:away][:players][1][:number] = 0
+  team[:away][:players][1][:shoe] = 16
   team[:away][:players][1][:points] = 12
-  team[:away][:players][1][:rebounds] = 12
-  team[:away][:players][1][:assists] = 12
-  team[:away][:players][1][:steals] = 12
-  team[:away][:players][1][:blocks] = 12
-  team[:away][:players][1][:slam_dunks] = 7
+  team[:away][:players][1][:rebounds] = 4
+  team[:away][:players][1][:assists] = 7
+  team[:away][:players][1][:steals] = 22
+  team[:away][:players][1][:blocks] = 15
+  team[:away][:players][1][:slam_dunks] = 10
 
   team[:away][:players][2][:player_name] = "Brook Lopez"
   team[:away][:players][2][:number] = 11
